425c986c74a5b6760d7d270722b4c26e90d189da
Mehul <119481480+Mehul6112@users.noreply.github.com>
2022-12-04 23:11:16 +0530

    version 1.4

--- freq.py
+++ freq.py
@@ -1,11 +1,18 @@
-my_string = str(input("Enter a string: "))
+l = True
+while l == True: 
+    my_string = str(input("Enter a string: "))
   
-freq = {}
+    freq = {}
   
-for i in my_string:
-    if i in freq:
-        freq[i] += 1
+    for i in my_string:
+        if i in freq:
+            freq[i] += 1
+        else:
+            freq[i] = 1
+  
+    print ("Count of all characters in", my_string, "is :","\n " +  str(freq))
+    l = input("Do you want to run the program again? (y/n): ")
+    if l == "y":
+        l = bool(1)
     else:
-        freq[i] = 1
-  
-print ("Count of all characters in", my_string, "is :","\n " +  str(freq))
+        l = bool(0)
