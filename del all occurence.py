0562aa030060dfa2c87243b8e978f19c8fdf9508
Mehul <119481480+Mehul6112@users.noreply.github.com>
2022-12-05 10:06:11 +0530

    version 1.5

--- del all occurence.py
+++ del all occurence.py
@@ -1,13 +1,12 @@
 str1 = str(input("Enter a sentence: "))
 result = ""
 ch_to_remove = str(input("input a char: "))
-occurred_flag = False
-# iterate over each character in s
+
 for ch in str1:
+    occurred_flag = False
     if ch == ch_to_remove and occurred_flag == False:
         occurred_flag = True
         continue
     else:
         result += ch
-# display the resulting string
 print(result)
