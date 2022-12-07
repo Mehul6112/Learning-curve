a7cd88251d9797537706608ba0152002c82c4c49
Mehul <119481480+Mehul6112@users.noreply.github.com>
2022-12-07 23:42:00 +0530

    version 1.7

---
+++ tuple operations.py
@@ -0,0 +1,8 @@
+t1 = (1,2,5,9,2,4,6,8,10)
+t2 = (11,13,15)
+print(t1[0:5], "\n")
+print(t1[5:10], "\n")
+print(t1[4:10], "\n")
+print(t3, "\n")
+print(t1+t2, "\n")
+print("Minimum value is: ", min(t1+t2), "\n", "Maximum value is: ", max(t1+t2))
